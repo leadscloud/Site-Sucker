0aca577c83d88253ba0d1c371ab32972f5fa5351
alexey <ceo@sistd.com>
2014-10-09 14:31:43 +0300

    update code, can download from css images and fonts

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-"Site Sucker" ver 0.5 python 2.7
+"Site Sucker" ver 0.7 python 2.7
 created by Alexey Sysoev, SI Studio
 
 enter url like this http://sistdTest.com/, 
@@ -12,11 +12,16 @@ what program does:
 - download all css
 - download all js
 - download all image
-
-
-what to do:
-- donwload all pages
 - download all fonts
 - download images from css
+
+todo:
+- donwload all pages
+
+problems:
+- dont suck files that strats from "http:"
+- folder with fonts may be not in the plase where css is searching it, so you can just copy/past in folder where css need
+- when you suck files that start with such url: "/", you try open index file and it dont see url "/" cose it it main folder of your pc. So you can just delete "/" or put files in web server
+- program can create empty files, that is type in css but there is no such file on server, so u can just delete it
 
 for all questions, write to info@sistd.com
--- web_suck.py
+++ web_suck.py
@@ -1,18 +1,23 @@
-﻿from lxml import html
+from lxml import html
 import requests
 import urllib
 import os, sys
+import re
 
 
-global tree,folder
+global tree,folder,url,log
 
-url = raw_input("Введите страничку: ")
-folder = raw_input("Введите название папки: ")
+url = raw_input("Enter web page to suck: ")
+folder = raw_input("Enter folder where to suck: ")
 if os.path.isdir(folder) == False:
     os.mkdir( folder, 0755 );
-    print "Папка создана"
+    log = folder+'/stat.log'
+    f = open(log, 'wb')
+    with open(log, 'a') as file:
+        file.write('Folder "' + folder +  '" created\n')
 else:
-    print "Такая папка уже есть"
+    with open(log, 'a') as file:
+        file.write('Such folder exist\n')
     
 page = requests.get(url)
 tree = html.fromstring(page.text)
@@ -24,10 +29,9 @@ def get_html(url,folder):
     f.write(data)
     f.close()
 
-def suck(rules,typeF):
+def suck(rules,typeF, typeS):
     fObject = tree.xpath(rules)
     if len(fObject) > 0:
-       
         for data in fObject:
             
             href = data.attrib[typeF]
@@ -36,25 +40,77 @@ def suck(rules,typeF):
             path_fObject = ""
             for data in range(0, index):
                 path_fObject = path_fObject + all_path[data] +"/"
-
+            
             if os.path.isdir(folder+"/"+path_fObject) == False:
                 os.makedirs(folder+"/"+path_fObject, 0755 );
-
-            print path_fObject + all_path[index]   
-                       
+                with open(log, 'a') as file:
+                    file.write('Folder "' + path_fObject + '" created.\n')
+            with open(log, 'a') as file:
+                file.write(path_fObject + all_path[index]+"\n")
             u = urllib.urlopen(str(url) + path_fObject + all_path[index])
             data = u.read()
             f = open(folder + '/'+path_fObject+ all_path[index], 'wb')
             f.write(data)
             f.close()
 
-print "Выполняется..."
+            #suck images and fonts only from css
+            if typeS == "css":
+                u = urllib.urlopen(str(url) + path_fObject + all_path[index])
+                data = u.read()
+                css_comments_removed = re.sub(r'\/\*.*?\*\/', '', data)
+                pattern = re.compile(r"url\('?(.*?\.[a-z]{3})")
+                matches = pattern.findall(css_comments_removed)  
+                
+                for i in matches:
+                    i = i.replace("'", "")
+                    valS = i.split("/")
+                    col = len(valS)
+                    if valS[0] == "..":
+                        full_image = ""
+                        for st in range(1, col):
+                            full_image = full_image + "/" +valS[st]
+                        if os.path.isfile(folder+"/"+full_image) == False:
+                            with open(log, 'a') as file:
+                                file.write("|--->" + full_image+"\n")
+                            ind = len(full_image.split("/")) - 1
+                            path_fObject = ""
+                            for data in range(0, ind):
+                                path_fObject = path_fObject + full_image.split("/")[data] +"/"
+                            if os.path.isdir(folder+"/"+path_fObject) == False:
+                                os.makedirs(folder+"/"+path_fObject, 0755 );
+                                with open(log, 'a') as file:
+                                    file.write('Folder "' + path_fObject + '" created.\n')
+                            u = urllib.urlopen(str(url) + full_image)
+                            data = u.read()
+                            f = open(folder + '/'+path_fObject+ full_image.split("/")[ind], 'wb')
+                            f.write(data)
+                            f.close()
+                    else:
+                        full_image = i
+                        if os.path.isfile(folder+"/"+full_image) == False:
+                            with open(log, 'a') as file:
+                                file.write("|--->" + full_image+"\n")
+                            
+                            ind = len(full_image.split("/")) - 1
+                            path_fObject = ""
+                            for data in range(0, ind):
+                                path_fObject = path_fObject + full_image.split("/")[data] +"/"
+                            if os.path.isdir(folder+"/"+path_fObject) == False:
+                                os.makedirs(folder+"/"+path_fObject, 0755 );                        
+                            u = urllib.urlopen(str(url) + full_image)
+                            data = u.read()
+                            f = open(folder + '/'+path_fObject+ full_image.split("/")[ind], 'wb')
+                            f.write(data)
+                            f.close()
+                        
+
+print "Working, wait..."
 
 get_html(url,folder)
 #suck js
-suck('//script[contains(@src, "/")][not(contains(@src, "http://"))][not(contains(@src, "//"))]', 'src')
+suck('//script[contains(@src, "/")][not(contains(@src, "http://"))][not(contains(@src, "//"))]', 'src','js')
 #suck css
-suck('//link[not(contains(@href, "http://"))][not(contains(@href, "//"))]','href')
+suck('//link[not(contains(@href, "http://"))][not(contains(@href, "//"))]','href','css')
 #suck img
-suck('//img[not(contains(@src, "http://"))][not(contains(@src, "//"))]','src')
-print "Выполнено!"
+suck('//img[not(contains(@src, "http://"))][not(contains(@src, "//"))]','src', 'img')
+print "Done!"
